5a71495705db39b422af524f8b5d87605b2a5ef9
Tr0ph1c <ahmmahmouda767@gmail.com>
2024-04-27 23:24:23 +0300

    added backend to index.php

--- index.php
+++ index.php
@@ -1,3 +1,41 @@
+<?php
+$inputs_file = fopen("inputs", "w") or die("cant open inputs");
+$outputs_file = fopen("outputs", "r") or die("cant open outputs");
+
+
+
+// SET VARIABLES
+$QT = 10;
+if (isset($_REQUEST["QTvar"])) {
+    $QT = $_REQUEST["QTvar"];
+}
+
+if($_SERVER['REQUEST_METHOD'] === 'GET' &&
+isset($_REQUEST["NOPvar"])) 
+{
+    $NOP = $_REQUEST["NOPvar"];
+    
+    for ($i = 0; $i < intval($NOP); $i++) {
+        if (isset($_GET["P${i}BT"])) fwrite($inputs_file, $_GET["P${i}BT"]." ".$_GET["P${i}AT"]."\n");
+    }
+}
+// END SET VARIABLES
+
+
+/*
+// GET ALL OUTPUTS FROM OUTPUTS FILE
+$raw_arr = array();
+while (!feof($outputs_file)) {
+    array_push($raw_arr, fgets($outputs_file));
+}
+// END GET ALL OUTPUTS FROM OUTPUTS FILE
+*/
+
+
+fclose($inputs_file);
+fclose($outputs_file);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -5,6 +43,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Round Robin Scheduling Algorithm Simulation</title>
         <link rel="stylesheet" href="style.css">
+        <link rel="icon" href="./imgs/icon.ico">
     </head>
 
     <body>
@@ -31,12 +70,12 @@
                 <div class="preq">
                     <div class="inp-box">
                         <label for="QT">Quantum time (ms): </label>
-                        <input type="text" id="QT">
+                        <input type="number" id="QT" min="0.1" max="1000" step="0.1" onchange="setQT();" value="<?php echo $QT; ?>">
                     </div>
     
                     <div class="inp-box">
                         <label for="NOP">Number of processes: </label>
-                        <input type="number" id="NOP" onchange="change();">
+                        <input type="number" id="NOP" min="1" max="100" step="1" onchange="setNOP();" value=<?php echo count(file('inputs')) ?>>
                     </div>
                 </div>
 
@@ -53,9 +92,17 @@
                         </tr>
                     </tbody>
                 </table>
-                <table id="s"></table>
+                <form action="index.php" method="get">
+                    <table id="s"></table>
+    
+                    <button style="margin-top: 10px;" id="btn">Start Simulation</button>
+                    <input type="hidden" id="NOPvar" name="NOPvar"/>
+                    <input type="hidden" id="QTvar" name="QTvar"/>
+                </form>
+            </div>
 
-                <button style="margin-top: 10px;" id="btn">Start Simulation</button>
+            <div class="section" id="gantt">
+                <h1>GANTT CHART</h1>
             </div>
         </div>
 
@@ -63,31 +110,39 @@
     </body>
 
     <script>
-        change();
-        let btn = document.getElementById("NOP");
-        function change() {
-            console.log("press");
+        setNOP();
+        setQT();
+
+        let NOP = document.getElementById("NOP");
+        let QT = document.getElementById("QT");
+
+        function setNOP() {
             let x = parseInt(document.getElementById("NOP").value);
-            console.log(x);
+            let NOPvar = document.getElementById("NOPvar");
+            NOPvar.value=x;
             if (x < 1 || isNaN(x) === true) x = 1;
             let y = "";
 
-            for(i = 0;i<x;i++){
-                y += "<tr>"
-                    y += "<td></td>"
-                    y += "<td class=edit><div id=editable contenteditable=true></div></td>"
-                    y += "<td class=edit><div id=editable contenteditable=true></div></td>"
-                    y += "<td></td>"
-                    y += "<td></td>"
-                    y += "<td></td>"
-                    y += "<td></td>"
-                y += "</tr>"
+            for(let i = 0; i < x; i++) {
+                y += "<tr>";
+                    y += `<td>P${i}</td>`;
+                    y += `<td class=edit><input style="width:65px;border:none;text-align:center;" type=number name="P${i}BT"></td>`;
+                    y += `<td class=edit><input style="width:65px;border:none;text-align:center;" type=number name="P${i}AT"></td>`;
+                    y += `<td id="CT${i}"></td>`;
+                    y += `<td id="WT${i}"></td>`;
+                    y += `<td id="TT${i}"></td>`;
+                    y += `<td id="RT${i}"></td>`;
+                y += "</tr>";
             }
 
             document.getElementById("s").innerHTML = y;
         }
         
-        
+        function setQT() {
+            let x = parseInt(document.getElementById("QT").value);
+            let NOPvar = document.getElementById("QTvar");
+            QTvar.value=x;
+        }
     </script>
 </html>
 
